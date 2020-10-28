dfd6df86b1ef5a6b149a1499c30a06fc86e58c31
Ssebi <sebidancau@yahoo.com>
2020-10-28 10:09:44 +0200

    cv download and resize fixed

--- app.js
+++ app.js
@@ -5,8 +5,7 @@ const port = process.env.PORT || 8080;
 const nodemailer = require('nodemailer');
 const bodyParser = require('body-parser');
 const fs = require('fs');
-require("dotenv").config();
-
+require('dotenv').config();
 
 app.use(express.static(path.join(__dirname, '/public')));
 
@@ -15,105 +14,91 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-
 app.listen(port);
 
 //Reading data from data.json
 
 //Routes
 app.get('/', (req, res) => {
-    const language = req.query.lng;
+  const language = req.query.lng;
 
-    if (language === 'ro') {
-        fs.readFile('data-ro.json', (error, data) => {
-            if (error) throw error;
-            let projects = JSON.parse(data);
-            res.render('index-romanian', {
-                language: 'ro',
-                projects
-            });
-        });
-    }
-    else {
-        fs.readFile('data-eng.json', (error, data) => {
-            if (error) throw error;
-            let projects = JSON.parse(data);
-            res.render('index', {
-                language: 'en',
-                projects
-            });
-        });
-    }
+  if (language === 'ro') {
+    fs.readFile('data-ro.json', (error, data) => {
+      if (error) throw error;
+      let projects = JSON.parse(data);
+      res.render('index-romanian', {
+        language: 'ro',
+        projects,
+      });
+    });
+  } else {
+    fs.readFile('data-eng.json', (error, data) => {
+      if (error) throw error;
+      let projects = JSON.parse(data);
+      res.render('index', {
+        language: 'en',
+        projects,
+      });
+    });
+  }
+});
 
-})
-
-
-
-
-
+app.get('/resume', (req, res) => {
+  res.download('./public/Cv.pdf', (err) => {
+    if (err) res.render('/');
+  });
+});
 
 app.get('/project', (req, res) => {
-    const id = req.query.id;
-    const language = req.query.lng;
-    if (language === 'en') {
-        fs.readFile('data-eng.json', (error, data) => {
-            if (error) throw error;
-            let projects = JSON.parse(data);
-            res.render('project',
-                {
-                    project: projects[id]
-                }
-            );
-        });
-    }
-    else if (language === 'ro') {
-        fs.readFile('data-ro.json', (error, data) => {
-            if (error) throw error;
-            let projects = JSON.parse(data);
-            res.render('project',
-                {
-                    project: projects[id]
-                }
-            );
-        });
-    }
-
-
-})
-
-
+  const id = req.query.id;
+  const language = req.query.lng;
+  if (language === 'en') {
+    fs.readFile('data-eng.json', (error, data) => {
+      if (error) throw error;
+      let projects = JSON.parse(data);
+      res.render('project', {
+        project: projects[id],
+      });
+    });
+  } else if (language === 'ro') {
+    fs.readFile('data-ro.json', (error, data) => {
+      if (error) throw error;
+      let projects = JSON.parse(data);
+      res.render('project', {
+        project: projects[id],
+      });
+    });
+  }
+});
 
 var transporter = nodemailer.createTransport({
-    host: process.env.host,
-    port: 587,
-    secure: false,
-    requireTLS: true,
-    auth: {
-        user: process.env.User,
-        pass: process.env.Password
-    }
+  host: process.env.host,
+  port: 587,
+  secure: false,
+  requireTLS: true,
+  auth: {
+    user: process.env.User,
+    pass: process.env.Password,
+  },
 });
 
 app.post('/message', (req, res) => {
-    const { email, subject, message } = req.body;
-    const language = req.query.lng;
-    var mailOptions = {
-        from: email,
-        to: 'sebidancau@yahoo.com',
-        subject: subject,
-        html: `From: ${email} <br>${message}`
-    };
+  const { email, subject, message } = req.body;
+  const language = req.query.lng;
+  var mailOptions = {
+    from: email,
+    to: 'sebidancau@yahoo.com',
+    subject: subject,
+    html: `From: ${email} <br>${message}`,
+  };
 
-    transporter.sendMail(mailOptions, function (error, info) {
-        if (error) {
-            console.log(error);
-        } else {
-            res.render('messageConfirmation', {
-                language
-            });
-        }
-    });
-
-})
-
-
+  transporter.sendMail(mailOptions, function (error, info) {
+    if (error) {
+      console.log(error);
+    } else {
+      res.render('messageConfirmation', {
+        language,
+      });
+    }
+  });
+});
